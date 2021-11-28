bac544d8be8d36cbc350d46b4da4460a43d5bce6
Nikita Baranov <baranovnikita18@gmail.com>
2021-11-28 15:53:52 +0300

    p09: remove RestTemplate's

--- src/main/kotlin/com/itmo/microservices/demo/order/api/external/IPaymentApi.java
+++
@@ -1,7 +0,0 @@
-package com.itmo.microservices.demo.order.api.external;
-
-import com.itmo.microservices.demo.order.api.dto.OrderDto;
-
-public interface IPaymentApi {
-    void pay(OrderDto orderDto);
-}
