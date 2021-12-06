416766f2654c2cc3e80292a8c68bcebe9680802a
Nikita Baranov <baranovnikita18@gmail.com>
2021-12-06 18:32:42 +0300

    p09: set timeCreated

--- src/main/kotlin/com/itmo/microservices/demo/order/impl/entity/OrderEntity.java
+++ src/main/kotlin/com/itmo/microservices/demo/order/impl/entity/OrderEntity.java
@@ -38,7 +38,7 @@ public class OrderEntity extends AbstractEntity {
     private List<OrderItemEntity> orderItems = new ArrayList<>();
 
     public OrderEntity() {
-
+        this.timeCreated = LocalDateTime.now();
     }
 
     @Override
