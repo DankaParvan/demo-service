b4ac65f4a80741ace90404017e07d4e39756df24
NoTimeLost <vvit.kozlov@icloud.com>
2022-03-22 16:07:15 +0300

    выпилил

--- src/test/java/com/itmo/microservices/shop/common/test/DataJpaTestCase.java
+++
@@ -1,8 +0,0 @@
-package com.itmo.microservices.shop.common.test;
-
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-@DataJpaTest
-public abstract class DataJpaTestCase {
-
-}
